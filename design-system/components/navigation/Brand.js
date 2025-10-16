9bed82f9da4d6555ca78f53a2b8a4a0af6ea1120
ali-ui-dev <ali.al-zuhairi@webropol.com>
2025-10-16 14:11:17 +0300

    Add simple mode to WebropolBrand and update logo styles
    Introduces a 'simple' attribute to the WebropolBrand component for a non-rotated logo variant. Updates styling in cp.html and login.html to use design tokens and improve logo appearance and alignment in the sidebar.

--- design-system/components/navigation/Brand.js
+++ design-system/components/navigation/Brand.js
@@ -6,29 +6,56 @@
 import { BaseComponent } from '../../utils/base-component.js';
 
 export class WebropolBrand extends BaseComponent {
+  static get observedAttributes() {
+    return ['simple'];
+  }
+
   connectedCallback() {
     super.connectedCallback();
     this.render();
   }
 
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (name === 'simple' && oldValue !== newValue) {
+      this.render();
+    }
+  }
+
   render() {
-    // Keep styling inherited from the host element's classes
-    this.innerHTML = `
-      <span class="webropol-brand-text inline-block">
-  <span style="font-size: 1.22em;">WEB</span>
-        <span class="webropol-brand-ro" style="
-          color: var(--accent-500);
-          display: inline-flex;
-          transform: rotate(-90deg);
-          transform-origin: center center;
-          line-height: 1;
-          font-size: 0.7em;
-          margin: -5px -2px 0px -8px;
-          padding-top: 9px;
-        ">RO</span>
-  <span style="font-size: 1.22em;">POL</span>
-      </span>
-    `;
+    const isSimple = this.hasAttribute('simple');
+    
+    if (isSimple) {
+      // Simple mode: just "Webropol" without rotation
+      this.innerHTML = `
+        <span class="webropol-brand-text" style="display:inline-flex; align-items:center; line-height:1;">
+          <span style="font-size: 1em; line-height:1;">WEB</span>
+          <span class="webropol-brand-ro" style="
+            color: var(--accent-500, #ff6429);
+            font-size: 1em;
+            line-height:1;
+          ">RO</span>
+          <span style="font-size: 1em; line-height:1;">POL</span>
+        </span>
+      `;
+    } else {
+      // Original mode with rotation
+      this.innerHTML = `
+        <span class="webropol-brand-text" style="display:inline-flex; align-items:center; line-height:1;">
+          <span style="font-size: 1.22em; line-height:1;">WEB</span>
+          <span class="webropol-brand-ro" style="
+            color: var(--accent-500, #ff6429);
+            display: inline-flex;
+            transform: rotate(-90deg);
+            transform-origin: center center;
+            line-height: 1;
+            font-size: 0.5em;
+            margin: 0 -3px;
+            padding: 0;
+          ">RO</span>
+          <span style="font-size: 1.22em; line-height:1;">POL</span>
+        </span>
+      `;
+    }
   }
 }
 
